df6174bea626d52957c20c949852b991ac7bd75c
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-04-25 11:25:10 -0300

    UPDATE

---
+++ projetos/Projeto Filmes em Cartaz/src/assets/js/script.js
@@ -0,0 +1,19 @@
+$(function (){
+    $.ajax ({
+        url:'https://api.b7web.com.br/cinema/',
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function(){
+            $('.filmes').html('<div class="col-md-12">Carregando...</div>');
+        },
+        success: function(json) {
+            // console.log(json);
+            var html = '';
+            for (var i in json) {
+                html += '<div class="col-md-4"><div class="filme"><img src="'+json[i].avatar+'"/>'+json[i].titulo+'</div></div>';
+            }
+
+            $('.filmes').html(html);
+        }
+    });
+});
